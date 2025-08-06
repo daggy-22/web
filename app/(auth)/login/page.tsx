89de3c8e93860c5686c229d14de18aa676e72fe5
Dagmawi <dagtt316@gmail.com>
2025-08-06 14:06:11 +0300

    Clean documentation only

--- app/(auth)/login/page.tsx
+++
@@ -1,6 +0,0 @@
-import LoginComponent from "@/components/login/Login";
-import SplitLayout from "@/components/ui/SplitLayout";
-
-export default function LoginPage() {
-  return <SplitLayout rightMain={<LoginComponent />} showHeader={false} />;
-}
--- app/(onboarding)/auth/reset-password/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import SetPassword from "@/components/auth/ResetPassword";
-import SplitLayout from "@/components/ui/SplitLayout";
-
-export default function SetNewPassword() {
-  return <SplitLayout showStepper={false} rightMain={<SetPassword />} />;
-}
